c27cdb705184d96fc4808e548f0ba0a95b886854
Arthur <arthurferreira1417@gmail.com>
2024-06-25 18:44:43 -0300

    Feat: criando helps de response

---
+++ api/app/helpers.php
@@ -0,0 +1,19 @@
+<?php
+
+if(!function_exists('jsonResponse')){
+    function jsonResponse($menssage, $status = 201, $data = []){
+        return response()->json([
+            'message'=> $menssage,
+            'data' =>$data,
+        ], $status);
+    }
+}
+if(!function_exists('jsonError')){
+    function jsonError($menssage, $status = 201){
+        return response()->json([
+            'message'=> $menssage,
+        ], $status);
+    }
+}
+
+
