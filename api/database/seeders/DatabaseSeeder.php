ab7def5f762a713c6f8fea1d48ef3228f6715cb5
Arthur <arthurferreira1417@gmail.com>
2024-06-25 19:33:42 -0300

    Feat: populando Comments em conjuto de article e user ja que eles depende um do outro

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Article;
+use App\Models\Comment;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -20,12 +21,14 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         // \App\Models\Article::factory(10)->create();
         User::factory(10)->create()->each(function ($user){
-            Article::factory(5)->create(['user_id' => $user->id]);
+            Article::factory(5)->create(['user_id' => $user->id])->each(function ($article) use ($user){
+                Comment::factory(3)->create(
+                ['user_id' => $user->id, 'article_id' => $article->id ]);
+
+            });
         });
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+
     }
 }
