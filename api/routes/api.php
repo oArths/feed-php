4535a172ddb3ffa17cf0e698129ea0b7f77b8498
Arthur <arthurferreira1417@gmail.com>
2024-06-25 18:45:17 -0300

    Feat: crinado os enpoints do crud de article

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\UserContrller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,12 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::post('/singup', [UserContrller::class, 'create_user'])->name('create.user');
 Route::post('/siguin', [UserContrller::class, 'login_user'])->name('login.user');
+
+Route::prefix('auth')->group(function(){
+Route::post('/articles', [ArticleController::class, 'create_article'])->name('create.article')->middleware('jwt');
+Route::get('/articles', [ArticleController::class, 'list_article'])->name('list.article')->middleware('jwt');
+Route::get('/articles/user/{id?}', [ArticleController::class, 'get_user_article'])->name('list.article')->middleware('jwt');
+Route::get('/articles/all/{id?}', [ArticleController::class, 'get_article'])->name('get.article')->middleware('jwt');
+Route::delete('/articles/delete/{id?}', [ArticleController::class, 'delete_article'])->name('delete.article')->middleware('jwt');
+Route::put('/articles/update/', [ArticleController::class, 'update_article'])->name('update.article')->middleware('jwt');
+});
