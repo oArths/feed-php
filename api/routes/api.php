5677f3089bd42dfe4553f71599dd8fb0986f831b
Arthur <arthurferreira1417@gmail.com>
2024-06-24 21:32:05 -0300

    Feat: criando ondpoints de login e siguin

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserContrller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::post('/singup', [UserContrller::class, 'create_user'])->name('create.user');
+Route::post('/siguin', [UserContrller::class, 'login_user'])->name('login.user')->middleware('jwt');
