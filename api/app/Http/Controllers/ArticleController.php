98afcf469d48fc991f4bb779bb40b739690e7d07
Arthur <arthurferreira1417@gmail.com>
2024-06-26 17:52:20 -0300

    Feat: criando o controllers prq pegar os articles que tem relação com as tags que o usuario goasta

--- api/app/Http/Controllers/ArticleController.php
+++ api/app/Http/Controllers/ArticleController.php
@@ -39,10 +39,13 @@ public function list_article(){
 
 public function get_user_article( $id = null){
 
-    if(is_null($id)){
-        return jsonError('Id não informado', 404);
+    $user = User::find($id);
+
+
+    if(is_null($id) || is_null($user)){
+        return jsonError('Id não informado ou invalido', 404);
     }
-    $articles = Article::where('user_id', $id)->get();
+    $articles = $user->articles()->get();
 
     if($articles->isEmpty()){
         return jsonError('Usuario não possui artigos', 404);
@@ -114,5 +117,48 @@ public function recently_article(){
     $articles = Article::orderBy('created_at', 'desc')->get();
 
     return jsonResponse('Artigos recentes', 200, $articles);
+}
+public function articleTagsUser($userId = null){
+
+    $getUser =  User::find($userId);
+
+    if(!$getUser){
+        return jsonError('Usuario não encontrado', 401);
+    }
+
+    // with('tags') -> faz o relacionamneto dos articgos com as tags e traz o artigo com a tag relaconada
+    //pluck('tags') -> extrai o campo tag dos artigos e cria uma coleção com as tags
+    // flatten() ->achata a coleção de coleçoes que excistia em apenas uma  com todas as tags
+    // unique('id') -> remove as duplicadas de tags mantendo apenas tags unicas
+    $tags = $getUser->articles()->with('tags')->get()->pluck('tags')->flatten()->unique('id');
+
+    $articles = Article::whereHas('tags', function($query) use ($tags){
+        $query->whereIn('tags.id', $tags->pluck('id'));
+    })->get();
+
+
+    return jsonResponse('artigos associados as tags', 201, $articles);
+
+}
+public function TagsArticleUser($userId = null){
+   $userExist = User::find($userId);
+
+   if(!$userExist || is_null($userExist)){
+        return jsonError('id inexistente ou inexistente');
+   }
+    //USEI o ".*." pra acessar todos os dados se um subArray que existia em tags
+    $tags = $userExist->articles()->with('tags')->get()->pluck('tags.*.pivot.tag_id')->flatten()->unique();
+
+    //procure em artigos "wherehas -> onde tem" tags e passo função anonima e que ela vai usar "use" $tags
+    // executei a $query e "whereIn -> em que" O id da tag fosse o mesmo das do usuario
+    // condicionei que "where ->  onde" o o user_id fosse diferente "!=" do usuario que faz requisição
+    // juntei com tags "with" pra poder utilizar no front end
+   $articles = Article::whereHas('tags', function($query) use ($tags){
+        $query->whereIn('tag_id', $a);
+   })->where('user_id', '!=', $userExist->id)->with('tags')->get();
+
+   return jsonResponse('Artigos relacionasdos as Tags', 200, $articles);
+
+
 }
 }
