53d51d5ce1fb917d5e7e687bbc8d2968f6a3d0cf
Arthur <arthurferreira1417@gmail.com>
2024-06-28 16:23:20 -0300

    Feat: populando o banco

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -20,40 +20,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-    
-        // $tags = Tag::factory(20)->create();
-        // $like = ArticleLike::factory(20)->create();
 
-        // User::factory(10)->create()->each(function ($user) use ($tags) {
-        //     Article::factory(5)->create(['user_id' => $user->id])->each(function ($article) use ($user, $tags) {
-        //         $article->tags()->attach($tags->random(3));
-        //         $article->likes()->attach($like->random(3));
-
-        //         Comment::factory(3)->create([
-        //             'user_id' => $user->id,
-        //             'article_id' => $article->id
-        //         ]);
-        //     });
-        // });
         $tags = Tag::factory(20)->create();
 
-        // Cria curtidas
-        $likes = ArticleLike::factory(20)->create();
-
         // Cria usuários
-        User::factory(10)->create()->each(function ($user) use ($tags, $likes) {
+        User::factory(10)->create()->each(function ($user) use ($tags) {
             // Cria artigos para cada usuário
-            Article::factory(5)->create(['user_id' => $user->id])->each(function ($article) use ($user, $tags, $likes) {
+            Article::factory(5)->create(['user_id' => $user->id])->each(function ($article) use ($user, $tags) {
                 // Associa um conjunto aleatório de tags a cada artigo
                 $article->tags()->attach($tags->random(3)->pluck('id')->toArray());
 
-                // Associa um conjunto aleatório de likes a cada artigo
-                $likes->random(3)->each(function ($like) use ($article) {
-                    $article->likes()->create([
-                        'user_id' => $like->user_id,
-                    ]);
-                });
-
+                // Cria likes para os artigos
+                ArticleLike::factory(3)->create([
+                    'article_id' => $article->id,
+                    'user_id' => $user->id,
+                ]);
                 // Cria comentários para os artigos
                 Comment::factory(3)->create([
                     'user_id' => $user->id,
