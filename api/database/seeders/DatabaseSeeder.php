d47513d5e04390c57fb4ecb5be0a5b12b32b4165
Arthur <arthurferreira1417@gmail.com>
2024-06-27 21:54:17 -0300

    Feat: populando as novas tabelas

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Article;
+use App\Models\ArticleLike;
 use App\Models\Comment;
 use App\Models\User;
 use App\Models\Tag;
@@ -19,22 +20,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create()->each(function ($user){
-        //     Article::factory(5)->create(['user_id' => $user->id])->each(function ($article) use ($user){
-        //         Comment::factory(3)->create(
-        //         ['user_id' => $user->id, 'article_id' => $article->id ]);
-        //         $tags = Tag::factory(3)->create();
-        //         $article->tags()->attach($tags);
+    
+        // $tags = Tag::factory(20)->create();
+        // $like = ArticleLike::factory(20)->create();
+
+        // User::factory(10)->create()->each(function ($user) use ($tags) {
+        //     Article::factory(5)->create(['user_id' => $user->id])->each(function ($article) use ($user, $tags) {
+        //         $article->tags()->attach($tags->random(3));
+        //         $article->likes()->attach($like->random(3));
+
+        //         Comment::factory(3)->create([
+        //             'user_id' => $user->id,
+        //             'article_id' => $article->id
+        //         ]);
         //     });
         // });
         $tags = Tag::factory(20)->create();
 
+        // Cria curtidas
+        $likes = ArticleLike::factory(20)->create();
+
         // Cria usuários
-        User::factory(10)->create()->each(function ($user) use ($tags) {
+        User::factory(10)->create()->each(function ($user) use ($tags, $likes) {
             // Cria artigos para cada usuário
-            Article::factory(5)->create(['user_id' => $user->id])->each(function ($article) use ($user, $tags) {
+            Article::factory(5)->create(['user_id' => $user->id])->each(function ($article) use ($user, $tags, $likes) {
                 // Associa um conjunto aleatório de tags a cada artigo
-                $article->tags()->attach($tags->random(3));
+                $article->tags()->attach($tags->random(3)->pluck('id')->toArray());
+
+                // Associa um conjunto aleatório de likes a cada artigo
+                $likes->random(3)->each(function ($like) use ($article) {
+                    $article->likes()->create([
+                        'user_id' => $like->user_id,
+                    ]);
+                });
 
                 // Cria comentários para os artigos
                 Comment::factory(3)->create([
