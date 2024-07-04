917b18b353e3aa7087b3261bef96aab51ad67f35
Arthur <arthurferreira1417@gmail.com>
2024-07-04 20:57:15 -0300

    Feat: ajusatndo a criação de usuarios pra gerar token validos

--- api/database/factories/UserFactory.php
+++ api/database/factories/UserFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use App\Http\Controllers\jwtController;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,15 +19,33 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        return [
+        do {
+            $email = $this->faker->unique()->safeEmail();
+        } while (User::where('email', $email)->exists());
+
+        $user = new \App\Models\User( [
             'username' => $this->faker->userName,
-            'email' => fake()->unique()->safeEmail(),
+            'email' => $email,
             'password' => bcrypt('password'), // ou use Hash::make('password')
             'image' => $this->faker->imageUrl(),
-            'token' => Str::random(60),
             'created_at' => now(),
             'updated_at' => now(),
+        ]);
+        $user->save();
+
+        $jwt = new jwtController;
+        $token = $jwt->Token($user->email);
+
+        return [
+            'username' => $user->username,
+            'email' => $user->email,
+            'password' => $user->password,
+            'image' => $user->image,
+            'token' => $token,
+            'created_at' => $user->created_at,
+            'updated_at' => $user->updated_at,
         ];
+
     }
 
     /**
