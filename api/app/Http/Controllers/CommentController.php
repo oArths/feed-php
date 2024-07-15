50763c2ba9a53f02df08e25c758265eef909ecad
Arthur <arthurferreira1417@gmail.com>
2024-07-15 18:58:52 -0300

    Faet: delegando a funções diferentes a ação de curtir e descurtir um comentario além de agregar mais informações ao consultar os comenatrios

--- api/app/Http/Controllers/CommentController.php
+++ api/app/Http/Controllers/CommentController.php
@@ -9,23 +9,49 @@ use App\Http\Requests\LikeCommentsRequest;
 use App\Http\Requests\CommentUpdateRequest;
 use App\Models\Comment;
 use App\Models\ArticleLike;
+use App\Models\Article;
 use App\Models\CommentsLike;
+use App\Models\User;
+
 
 class CommentController extends Controller
 {
     public function list_comment($params = null){
 
+        $email = request();
+        $user = User::where('email', $email->Auth['email'])->first();
+        $userId =  $user['id'];
+        
         if(is_null($params)){
             return jsonResponse('É necessario infromar o id');
         } 
 
-        $comment = Comment::where('article_id', $params)->with('replies')->withCount('likesComments')->get();
+        $article = Article::where('id', $params)->withCount('likes')->withCount(['likes', 'comments'])->with(['user' => function($query){
+            $query->select('id', 'username', 'image');
+        }])->get();
+        
+        $article->each(function($article) use ($userId) {
+            $article->liked_by_user = $article->likes->contains('user_id', $userId);
+            unset($article->likes);
+        });
+        $comment = Comment::where('article_id', $params)->with(['replies', 'likesComments'])->withCount('likesComments')->with(['user' => function($query){
+            $query->select('id', 'username', 'image');
+        }])->get();
 
+        $comment->each(function($comment) use ($userId){
+            $comment->like_by_user_comment = $comment->likesComments->contains('user_id', $userId);
+            unset($comment->likesComments);
+        });
 
-        if($comment->isEmpty()){
-            return jsonResponse('Comentario inexistente');
-        }
-        return jsonResponse('Comentarios do artigo', 200 , $comment);
+        // if($comment->isEmpty()){
+        //     return jsonResponse('Comentario inexistente');
+        // }
+
+        $results = [
+            'article' => $article,
+            'comments' => $comment
+        ];
+        return jsonResponse('Comentarios do artigo', 200 , $results);
 
     }
     public function Create_Comment(CommentRrequest $prams){
@@ -77,19 +103,33 @@ class CommentController extends Controller
 
         $exist = CommentsLike::where('article_id', $parms->article_id)->where('user_id', $parms->user_id)->where('comment_id', $parms->comment_id,)->first(); 
 
-        if($exist){
-            $exist->delete();
-            return jsonResponse('Like apagado com Sucesso', 201);
-
-        }
-        if(empty($exist)){
+        if(!$exist){
             $like = CommentsLike::create([
                 'article_id' => $parms->article_id,
                 'user_id' => $parms->user_id,
                 'comment_id' => $parms->comment_id,
             ]);
-            return jsonResponse('Artigo Curtido com Sucesso', 201, $like);
+            return jsonResponse('Like criado com Sucesso', 201);
+
         }
+           
+        return jsonResponse('like de comenatrio ja existe', 201);
 
     }
+    public function likeCommentDelete(LikeCommentsRequest $parms){
+
+
+        $exist = CommentsLike::where('article_id', $parms->article_id)->where('user_id', $parms->user_id)->where('comment_id', $parms->comment_id,)->first(); 
+
+        if($exist){
+            $exist->delete();
+            return jsonResponse('Like apagado com Sucesso', 201);
+
+        }
+  
+        return jsonResponse('comentario ja foi curtido', 200);
+        
+
+    }
+
 }
